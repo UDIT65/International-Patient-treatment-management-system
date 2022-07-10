0184797e12b775dc72a62cf61787b15852b85135
Udit65 <uditgoyal65@gmail.com>
2022-07-10 11:20:27 +0530

    Intial commit

---
+++ src/test/java/com/cognizant/treatments/IpTreatmentsMicroserviceApplicationTests.java
@@ -0,0 +1,21 @@
+package com.cognizant.treatments;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+/**
+ * 
+ *
+ * 		 With the @SpringBootTest annotation, Spring Boot provides a 
+ * 		 convenient way to start up an application context to be used in a test
+ */
+@SpringBootTest
+class IpTreatmentsMicroserviceApplicationTests {
+
+@Test
+void contextLoads() {
+	
+}
+
+}
+
