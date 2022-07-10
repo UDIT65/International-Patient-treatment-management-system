c7138ff293bd04dfc69d173de887129bb454f2e6
Udit65 <uditgoyal65@gmail.com>
2022-07-10 11:16:11 +0530

    Intial commit

---
+++ src/main/java/com/cognizant/offerings/exception/PackageDetailNotFoundException.java
@@ -0,0 +1,25 @@
+package com.cognizant.offerings.exception;
+
+/**
+ * 
+ * 
+ * 		custom based exception class for PackageDetailNotFoundException
+ *
+ */
+public class PackageDetailNotFoundException  extends RuntimeException{
+
+	/**
+	 * 
+	 */
+	private static final long serialVersionUID = 1L;
+
+	public PackageDetailNotFoundException(String packageName) {
+		super("There is no package with- "+packageName);
+	}
+	
+	public PackageDetailNotFoundException(){
+		
+	}
+
+	
+}
---
+++ src/main/java/com/cognizant/offerings/model/PackageDetail.java
@@ -0,0 +1,34 @@
+package com.cognizant.offerings.model;
+
+import javax.persistence.Embeddable;
+
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+/**
+ * 
+ * 
+ * 		model class for PackageDetail
+ * 
+ * 		@Entity indicates Spring Data JPA that it is an entity class for the
+ *      application
+ *      @Data is a convenient shortcut annotation that bundles 
+ *      the features of @ToString , @EqualsAndHashCode , @Getter / @Setter 
+ *      and @RequiredArgsConstructor together
+ *
+ */
+@Embeddable
+@Data
+@AllArgsConstructor
+@NoArgsConstructor
+public class PackageDetail {
+
+	/**
+	 * instance variables
+	 */
+	private String treatmentPackageName;
+	private String testDetails;
+	private int cost;
+	private int treatmentDurationInWeeks;
+}
