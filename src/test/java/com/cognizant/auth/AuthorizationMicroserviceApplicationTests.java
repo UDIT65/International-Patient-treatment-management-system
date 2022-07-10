dc9cc42e810ffb43bbed0e225f8beb791ffaed19
Udit65 <uditgoyal65@gmail.com>
2022-07-10 20:41:50 +0530

    commit

---
+++ src/test/java/com/cognizant/auth/AuthorizationMicroserviceApplicationTests.java
@@ -0,0 +1,20 @@
+package com.cognizant.auth;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+/**
+ * 
+ * 
+ * 		 With the @SpringBootTest annotation, Spring Boot provides a 
+ * 		 convenient way to start up an application context to be used in a test
+ *
+ */
+@SpringBootTest
+class AuthorizationMicroserviceApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
